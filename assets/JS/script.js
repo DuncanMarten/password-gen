ab92014500de201c73c4cad3a72bfef977984582
Duncan <duncanmarten@yahoo.com>
2020-09-17 16:03:11 -0500

    added generator functions

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -12,3 +12,25 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+//Generator Functions
+
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+    const symbolStr = "!@#$%^&*()?<>|~?";
+    return symbolStr[Math.floor(Math.random() * symbolStr.length)];
+}
