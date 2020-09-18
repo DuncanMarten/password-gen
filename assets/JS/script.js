016e661c7c5c0f2256b616c22e8f77396d83cbf1
Duncan <duncanmarten@yahoo.com>
2020-09-18 13:33:23 -0500

    cleaned up js code and added for loop to loop through characters

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -7,18 +7,11 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+    
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-var randomFunc = {
-    lower: getRandomLower,
-    upper: getRandomUpper,
-    number: getRandomNumber,
-    symbol: getRandomSymbol
-}
 
 // Prompt for password length
 var lengthAccept = function() {
@@ -34,29 +27,37 @@ var lengthAccept = function() {
     return lengthNumber;
 }
 
-function randomNumber() {
-    var random = Math.floor(Math.random()*4 + 1);
-    return random;
-}
-
-console.log(randomNumber());
 // Generate password function
 function generatePassword(lower, upper, number, symbol, length) {
     // Initial password string
     var genPass = "";
     var length = lengthAccept();
     var lowerPrompt = confirm("Do you want lowercase letters?");
+        console.log(lowerPrompt);
+        
     var upperPrompt = confirm("Do you want uppercase letters?");
+        console.log(upperPrompt);
+        
     var numberPrompt = confirm("Do you want numbers?");
+        console.log(numberPrompt);
+
     var symbolPrompt = confirm("Do you want special characters?");
+        console.log(symbolPrompt);
+
+    
+        
 
     var charAccept = lowerPrompt + upperPrompt + numberPrompt + symbolPrompt;
-    //console.log('charAccept ', charAccept);
+    
 
     var charArr = [{lowerPrompt}, {upperPrompt}, {numberPrompt}, {symbolPrompt}].filter(
         item => Object.values(item) [0]
     );
-    //console.log('charArr ', charArr);
+    
+    //var charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890~!@#$%^&*()_+|?><";
+
+    var genPass = "";
+
 
     if (charAccept === 0) {
         return generatePassword();
@@ -65,26 +66,7 @@ function generatePassword(lower, upper, number, symbol, length) {
     //debugger;
     // Loop through to get password
     for (var i = 0; i < length; i++) {
-        //var getRandomCharacter = ;
+        genPass = genPass + charSet.charAt(Math.floor(Math.random() * Math.floor(charSet.length - 1)));
     }
-}
-
-
-//Generator Functions
-
-function getRandomLower() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-function getRandomUpper() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-function getRandomNumber() {
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function getRandomSymbol() {
-    const symbolStr = "!@#$%^&*()?<>|~?";
-    return symbolStr[Math.floor(Math.random() * symbolStr.length)];
+    return genPass;
 }
